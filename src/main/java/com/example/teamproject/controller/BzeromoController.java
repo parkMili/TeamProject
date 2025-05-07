28de6fe7b76fa9a0a87d5fa27fa5555212b62f55
bzeromo <dudrb5260@naver.com>
2025-05-07 13:02:06 +0900

    Update BzeromoController

--- src/main/java/com/example/teamproject/controller/BzeromoController.java
+++ src/main/java/com/example/teamproject/controller/BzeromoController.java
@@ -7,11 +7,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BzeromoController {
+    private int num;
     private String[]lunch = {"부대찌개", "마라탕", "햄버거", "짜장면", "짬뽕"};
 
     @GetMapping("/bzeromo")
     public @ResponseBody String getLunch() {
         String ans = "굶어야할듯요";
+        num = (int)(Math.random() * 100);
+
+        if(num < 5 && num >=0)
+            ans = lunch[num];
 
         return ans;
     }
